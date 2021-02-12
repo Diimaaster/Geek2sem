a6f11361f0c7bc3803ba69a666edecb041a46feb
Diimaaster <khrabandm@mail.ru>
2021-02-12 10:18:50 +0300

    homework_3_and_4

---
+++ Hw3-4/app/src/main/java/com/example/calculater/MainActivity2.java
@@ -0,0 +1,72 @@
+package com.example.calculater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private String AppTheme;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button btnReturn = findViewById(R.id.btnReturn);
+        setTheme(getAppTheme(R.style.MyCoolStyle));
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Метод finish() завершает активити
+                finish();
+            }
+        });
+    }
+
+    private void initRadioButton(View button, final int codeStyle){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // сохраним настройки
+                setAppTheme(codeStyle);
+                // пересоздадим активити, чтобы тема применилась
+                recreate();
+            }
+        });
+    }
+
+    private int getAppTheme(int codeStyle) {
+        return codeStyleToStyleId(getCodeStyle(codeStyle));
+    }
+    private static final String NameSharedPreference = "LOGIN";
+    // Чтение настроек, параметр «тема»
+    private int getCodeStyle(int codeStyle){
+        // Работаем через специальный класс сохранения и чтения настроек
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
+        //Прочитать тему, если настройка не найдена - взять по умолчанию
+        return sharedPref.getInt(AppTheme, codeStyle);
+    }
+
+    // Сохранение настроек
+    private void setAppTheme(int codeStyle) {
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
+        // Настройки сохраняются посредством специального класса editor.
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(AppTheme, codeStyle);
+        editor.apply();
+    }
+    private static final int AppThemeCodeStyle = 0;
+    private int codeStyleToStyleId(int codeStyle){
+        switch(codeStyle){
+            case AppThemeCodeStyle:
+                return R.style.MyCoolStyle2;
+            default:
+                return R.style.MyCoolStyle;
+        }
+    }
+}
